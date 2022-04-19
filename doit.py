747268d19b3c7aedcb9a24831bb20fed73e0bb8e
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-19 13:28:09 -0400

    updating pipeline

--- doit.py
+++ doit.py
@@ -1,1 +1,8 @@
 python upload_video.py --file="output.mov" --title="Summer vacation in California" --description="Had fun surfing in Santa Cruz" --keywords="surfing,Santa Cruz" --category="22" --privacyStatus="private"
+
+#curl --request POST \
+#  'https://youtube.googleapis.com/youtube/v3/thumbnails/set?videoId=w2IBYm40-wo&key=755483159169-mhnrl5ucjoml7h1dgsnklnms19o18m1j.apps.googleusercontent.com' \
+#  --header 'Authorization: Bearer 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' \
+#  --header 'Accept: application/json' \
+#  --compressed
+
